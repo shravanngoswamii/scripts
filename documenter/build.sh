8c2b86aa14fb67fd1d5713fa080775435a5bcfc1
Shravan Goswami <123811742+shravanngoswamii@users.noreply.github.com>
2025-05-23 12:29:37 +0530

    Update build.sh

--- documenter/build.sh
+++ documenter/build.sh
@@ -8,16 +8,19 @@ echo "julia --project=docs/ -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd()));
 echo "julia --project=docs/ docs/make.jl"
 
 # Execute the Julia setup commands
+echo "Running Julia package setup..."
 julia --project=docs/ -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.instantiate()' 
 if [ $? -ne 0 ]; then
-    echo "Documentation build failed during instantiation step"
+    echo "Documentation build failed during instantiation step."
     exit 1
 fi
+echo "Julia package setup completed successfully."
 
+# Build the documentation
+echo "Running Julia documentation build..."
 julia --project=docs/ docs/make.jl
 if [ $? -ne 0 ]; then
     echo "Documentation build failed during documentation build step."
     exit 1
 fi
-
 echo "Documentation build succeeded!"
